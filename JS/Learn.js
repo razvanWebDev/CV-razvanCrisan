4b49b5511fcafa48262c1c85aba7e7103e5f99cb
Razvan Crisan <43964483+Crsnrazvan84@users.noreply.github.com>
2018-10-29 20:59:19 +0200

    ad exchange

--- JS/Learn.js
+++ JS/Learn.js
@@ -1,5 +1,9 @@
 console.info(0);
 
+function sayWelcome() {
+  console.info("Salut " + prenume + ", bine ai venit la noi!");
+}
+
 var nume = "Crisan";
 var prenume = "Razvan";
 var owner = "Razvan's";
@@ -9,6 +13,22 @@ var template = `Text cu ghilimele " sau simple'`;
 
 var skills = ["html", "css", "js"];
 
+prenume = "Razvi";
+
 console.info("toate variabilele au fost initializate");
 console.log(nume);
 console.debug("Cum te cheama? " + prenume);
+sayWelcome();
+
+// schimb valutar
+var cursEUR = 4.5;
+var myEUR = 20;
+var myRON = 0;
+var friendEUR = 10;
+var friendRON = 0;
+
+myRON = myEUR * cursEUR;
+console.info("eu am " + myRON + "RON");
+
+friendRON = friendEUR * cursEUR;
+console.info("eu am " + friendRON + "RON");
