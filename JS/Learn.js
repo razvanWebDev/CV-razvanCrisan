5db93f51613f28d2ece3d6ad7be2d83faa2f58dd
Razvan Crisan <43964483+Crsnrazvan84@users.noreply.github.com>
2018-10-29 21:43:38 +0200

    ad extractATM

--- JS/Learn.js
+++ JS/Learn.js
@@ -24,11 +24,26 @@ sayWelcome();
 var cursEUR = 4.5;
 var myEUR = 20;
 var myRON = 0;
-var friendEUR = 10;
+var friendEUR = 100;
 var friendRON = 0;
 
 myRON = myEUR * cursEUR;
-console.info("eu am " + myRON + "RON");
+console.info("eu am " + myRON + " RON");
 
-friendRON = friendEUR * cursEUR;
-console.info("eu am " + friendRON + "RON");
+friendRON = friendEUR * cursEUR * 0.99;
+console.info("tu ai " + friendRON + " RON");
+
+function extractFromATM(ammount) {
+  console.info("==== ==== ==== ==== ====");
+  console.info("suma extrasa este: " + ammount);
+  var comision = ammount * 0.01;
+  if (comision < 2.5) {
+    comision = 2.5;
+    console.warn("comision minim aplicat");
+  }
+  console.info("comision aplicat: " + comision);
+  console.info("==== ==== ==== ==== ====");
+}
+
+extractFromATM(1000);
+extractFromATM(100);
