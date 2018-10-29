5dc7f5303fe9f33de412d847a8fdadc7f87374f5
Razvan Crisan <43964483+Crsnrazvan84@users.noreply.github.com>
2018-10-29 20:26:41 +0200

    ad string variables

---
+++ JS/Learn.js
@@ -0,0 +1,14 @@
+console.info(0);
+
+var nume = "Crisan";
+var prenume = "Razvan";
+var owner = "Razvan's";
+var msg1 = "Ai zis Crisan";
+var msg2 = 'Nu, am zis "Razvan"';
+var template = `Text cu ghilimele " sau simple'`;
+
+var skills = ["html", "css", "js"];
+
+console.info("toate variabilele au fost initializate");
+console.log(nume);
+console.debug("Cum te cheama? " + prenume);
