327e7a669822fd84c69a2c0746895d3777ae97f6
Razvan Crisan <43964483+Crsnrazvan84@users.noreply.github.com>
2018-11-05 20:15:29 +0200

    Fix ExtraxtFrom Atm funds

--- JS/Learn.js
+++ JS/Learn.js
@@ -33,17 +33,80 @@ console.info("eu am " + myRON + " RON");
 friendRON = friendEUR * cursEUR * 0.99;
 console.info("tu ai " + friendRON + " RON");
 
-function extractFromATM(ammount) {
-  console.info("==== ==== ==== ==== ====");
-  console.info("suma extrasa este: " + ammount);
+function extract2FromATM(amount) {
+  var disponibil = 1000;
+  var comision = amount * 0.01;
+  var sumaRamasa = disponibil - amount - comision;
+
+  if (amount % 10 !== 0) {
+    console.info("==== ==== ==== ==== ====");
+    console.warn("Suma extrasa tebuie sa fie multiplu de 10!");
+    console.info("==== ==== ==== ==== ====");
+  } else {
+    console.info("==== ==== ==== ==== ====");
+
+    if (disponibil < amount + comision) {
+      console.warn("Fonduri insuficiente!");
+    } else {
+      if (comision < 2.5) {
+        comision = 2.5;
+        console.warn("comision minim aplicat");
+      }
+
+      console.info("suma extrasa este: " + amount + " Lei");
+      console.info("comision aplicat: " + comision);
+      console.info("Suma ramasa in cont: " + sumaRamasa + " Lei");
+    }
+    console.info("==== ==== ==== ==== ====");
+  }
+  amount = sumaRamasa; // de rezolvat
+}
+
+extract2FromATM(600);
+
+var atmFunds = 50000;
+var funds = 5000; // var globala (nu a fost definita in int functiei)
+
+function getExtraxtFee(ammount) {
   var comision = ammount * 0.01;
   if (comision < 2.5) {
     comision = 2.5;
     console.warn("comision minim aplicat");
   }
+  return comision;
+}
+// TODO implement
+function checkExtractPermissions() {
+  return true;
+}
+
+function extractFromATM(ammount) {
+  console.info("==== ==== ==== ==== ====");
+  console.info("suma extrasa este: " + ammount);
+  var comision = getExtraxtFee(ammount);
+
+  var totalExtract = ammount + comision;
+
+  if (totalExtract > funds) {
+    console.error("insufficient funds");
+    return;
+  }
+  if (atmFunds < totalExtract) {
+    console.error("ATM insufficient funds!");
+    return;
+  }
+
+  funds = funds - totalExtract;
+  atmFunds = atmFunds - ammount;
+  // atmFunds -= ammount;   versiunea simplificata pt operatiunea de mai sus
+
   console.info("comision aplicat: " + comision);
+  console.info("Sold curent: " + funds);
   console.info("==== ==== ==== ==== ====");
 }
 
-extractFromATM(1000);
+extractFromATM(1005);
+extractFromATM(100000);
+extractFromATM(3000);
+extractFromATM(3000);
 extractFromATM(100);
