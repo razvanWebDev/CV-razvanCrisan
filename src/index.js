5cfefc3649bfa72b7a3a7cf459b763f5b123f503
Crisan Razvan <crsn_razvan@yahoo.com>
2018-11-19 21:33:54 +0200

    add array.forEach

--- src/index.js
+++ src/index.js
@@ -13,9 +13,10 @@ function show (id) {
 
 function hideAllPages () {
   var pages = document.querySelectorAll(".page-block");
-  for (var i = 0; i < pages.length; i++) {
-    pages[i].style.display = "none";
-  }
+  pages.forEach(function(page){
+    page.style.display = "none";
+  })
+
 }
 
 var links = document.querySelectorAll("#top-menu-bar a");
@@ -23,7 +24,6 @@ for (var i = 0; i < links.length; i++ ) {
   links[i].onclick = function(){
     hideAllPages();
     var page = this.getAttribute("data-page");
-    console.info("show page" , page);
     show(page + "-page");
   };
 }
