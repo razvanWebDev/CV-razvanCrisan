83e9a25bb24f07c1412563eafe3b57d0df6a2a7f
Crisan Razvan <crsn_razvan@yahoo.com>
2018-11-26 21:47:50 +0200

    moved skills items in JS

--- src/index.js
+++ src/index.js
@@ -6,26 +6,42 @@ function hide(id) {
   $(id).style.display = "none";
 }
 
-function show (id) {
+function show(id) {
   $(id).style.display = "block";
 }
 
-function hideElement (el) {
+function hideElement(el) {
   el.style.display = "none";
 }
 
-function hideAllPages () {
+function hideAllPages() {
   var pages = document.querySelectorAll(".page-block");
   pages.forEach(hideElement);
 }
 
-var links = document.querySelectorAll("#top-menu-bar a");
-for (var i = 0; i < links.length; i++ ) {
-  links[i].onclick = function(){
-    hideAllPages();
-    var page = this.getAttribute("data-page");
-    show(page + "-page");
-  };
+function initMenu() {
+  var links = document.querySelectorAll("#top-menu-bar a");
+  for (var i = 0; i < links.length; i++) {
+    links[i].onclick = function () {
+      hideAllPages();
+      var page = this.getAttribute("data-page");
+      show(page + "-page");
+    };
+  }
 }
 
-show("home-page");
+function initSkillsPage() {
+  var skills = ['js', 'html', 'css'];
+  var resultList = document.querySelector('#skills-page ul');
+
+var listItems = skills.map(function(skill) {
+  return `<li>${skill.toUpperCase()}</li>`;
+})
+
+  
+  resultList.innerHTML = listItems.join('');
+}
+
+initMenu();
+show("skills-page");
+initSkillsPage();
