257dd4fe622aa7fa0e2cd93934df669a07665955
Crisan Razvan <crsn_razvan@yahoo.com>
2018-12-03 21:08:54 +0200

    use Jquery

--- src/index.js
+++ src/index.js
@@ -1,47 +1,27 @@
-function $(id) {
-  return document.getElementById(id);
-}
-
-function hide(id) {
-  $(id).style.display = "none";
-}
-
-function show(id) {
-  $(id).style.display = "block";
-}
-
-function hideElement(el) {
-  el.style.display = "none";
-}
-
-function hideAllPages() {
-  var pages = document.querySelectorAll(".page-block");
-  pages.forEach(hideElement);
-}
-
 function initMenu() {
   var links = document.querySelectorAll("#top-menu-bar a");
   for (var i = 0; i < links.length; i++) {
     links[i].onclick = function () {
-      hideAllPages();
+      $('.page-block').hide();
       var page = this.getAttribute("data-page");
-      show(page + "-page");
+      $('#' + page + '-page').show();
     };
   }
 }
 
 function initSkillsPage() {
   var skills = [
-    {name: 'js', endorsments: 7, endorsedBy: 'Andrei'},
-    {name: 'HTML', endorsments: 6, endorsedBy: ''},
-    {name: 'css', endorsments: 2, endorsedBy: ''},
+    { name: 'js', endorsments: 7, endorsedBy: 'Andrei' },
+    { name: 'HTML', endorsments: 6, endorsedBy: '' },
+    { name: 'css', endorsments: 2, endorsedBy: '' },
   ];
   var resultList = document.querySelector('#skills-page ul');
 
   var listItems = skills.map(function (skill) {
     var endorsedBy = " - Endorsed by";
-    if (skill.endorsedBy == "") { endorsedBy = "";
-   }
+    if (skill.endorsedBy == "") {
+      endorsedBy = "";
+    }
 
     var name = skill.name.toUpperCase();
 
@@ -54,5 +34,5 @@ function initSkillsPage() {
 }
 
 initMenu();
-show("skills-page");
+$('#skills-page').show();
 initSkillsPage();
