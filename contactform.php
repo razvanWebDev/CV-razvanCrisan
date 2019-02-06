f413275408b3377d2ce31dfc718ccaf27a54c256
Crisan Razvan <crsn_razvan@yahoo.com>
2019-02-06 16:36:49 +0200

    created contactform.php

--- contactform.php
+++ contactform.php
@@ -1,16 +1,15 @@
-<?php
+<?php 
 
-if(isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $mailFrom = $_POST['mail'];
-    $message = $_POST['message'];
+ $name = $_POST['name'];
+ $mailFrom = $_POST['mail'];
+ $message = $_POST['message'];
 
-    $mailTo = "crsn_razvan@yahoo.com";
-    $headers = "From: ".$mailFrom;
-    $txt = "You have received an email from ".$name.".\r\n ".$message;
+ $mailTo = "crsn_razvan@yahoo.com";
+ $headers = "From: ".$mailFrom;
+ $txt = "You have received an email from ".$name.".\n ".
+            "User message: $message.\n";
 
-    mail($mailTo, $txt, $headers);
-    header("Location: index.html");
-}
+ mail($mailTo,$txt,$headers);
+ header("Location: index.html");
 
 ?>
